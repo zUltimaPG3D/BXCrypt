a5e9f80e354377e9edbb1398dc851d943315d4b1
zUltimaPG3D <ggyuuhallo233@proton.me>
2023-12-13 14:08:22 +0100

    style: accurate field naming

--- Crypt.cs
+++ Crypt.cs
@@ -9,13 +9,13 @@ namespace BX.App.Util
 	{
 		public static void Initialize(string key, string iv, string head)
 		{
-			Key = key;
-			IV = iv;
-			plainTextHead = head;
+			m_Key = key;
+			m_IV = iv;
+			m_plainTextHead = head;
 		}
 		public static string Encrypt(string uid)
 		{
-			StringBuilder sb = new StringBuilder(plainTextHead);
+			StringBuilder sb = new StringBuilder(m_plainTextHead);
 			sb.Append(uid);
 			using (Rijndael rijn = RijndaelManaged.Create())
 			{
@@ -23,7 +23,7 @@ namespace BX.App.Util
 				rijn.Mode = CipherMode.CBC;
 				rijn.KeySize = 256;
 				rijn.BlockSize = 128;
-				ICryptoTransform encryptor = rijn.CreateEncryptor(Encoding.UTF8.GetBytes(Key), Encoding.UTF8.GetBytes(IV));
+				ICryptoTransform encryptor = rijn.CreateEncryptor(Encoding.UTF8.GetBytes(m_Key), Encoding.UTF8.GetBytes(m_IV));
 				MemoryStream memoryStream = new MemoryStream();
 				CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
 				cryptoStream.Write(Encoding.UTF8.GetBytes(sb.ToString()));
@@ -39,14 +39,14 @@ namespace BX.App.Util
 				rijn.Mode = CipherMode.CBC;
 				rijn.KeySize = 256;
 				rijn.BlockSize = 128;
-				ICryptoTransform decryptor = rijn.CreateDecryptor(Encoding.UTF8.GetBytes(Key), Encoding.UTF8.GetBytes(IV));
+				ICryptoTransform decryptor = rijn.CreateDecryptor(Encoding.UTF8.GetBytes(m_Key), Encoding.UTF8.GetBytes(m_IV));
 				MemoryStream memoryStream = new MemoryStream(Convert.FromBase64String(src));
 				CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
 				StreamReader streamReader = new StreamReader(cryptoStream);
 				string decrypted = streamReader.ReadToEnd();
-				if (decrypted.IndexOf(plainTextHead, StringComparison.CurrentCultureIgnoreCase) != -1)
+				if (decrypted.IndexOf(m_plainTextHead, StringComparison.CurrentCultureIgnoreCase) != -1)
 				{
-					decrypted = decrypted.Substring(plainTextHead.Length);
+					decrypted = decrypted.Substring(m_plainTextHead.Length);
 					decrypted = Regex.Replace(decrypted, "[\u0000-\u0019+/]", "");
 				}
 				memoryStream.Dispose();
@@ -55,8 +55,8 @@ namespace BX.App.Util
 				return decrypted;
 			}
 		}
-		public static string Key = "AehiP1hohphe4ith6ievoh4saht3aeca";
-		public static string IV = "eiGadaegungoo0gu";
-		public static string plainTextHead = "bx70test1";
+		public static string m_Key = "AehiP1hohphe4ith6ievoh4saht3aeca";
+		public static string m_IV = "eiGadaegungoo0gu";
+		public static string m_plainTextHead = "bx70test1";
 	}
 }
